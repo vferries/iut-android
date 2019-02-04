3df583cb854243969fd89054188c2b8f744bef18
Vincent FERRIES <vincent.ferries@gmail.com>
2019-02-04 22:28:10 +0100

    solution tp1

--- tps/tp1/app/src/main/java/android/iut/tp1/SearchActivity.java
+++ tps/tp1/app/src/main/java/android/iut/tp1/SearchActivity.java
@@ -2,6 +2,10 @@ package android.iut.tp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,9 +13,21 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // TODO 1 - inflate du fichier de layout dans l'activité
+        setContentView(R.layout.activity_main);
 
         // TODO 16 - Récupérer une référence au bouton et au champ de texte
+        Button search = findViewById(R.id.search);
+        // On doit la mettre final pour y accéder dans le click listener
+        final EditText name = findViewById(R.id.name);
         // TODO 17 - Ajouter un événement onClick sur le bouton
-        // TODO 18 - Dedans, afficher un Toast avec un message contenant le contenu du champ saisi
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO 18 - Dedans, afficher un Toast avec un message contenant le contenu du champ saisi
+                // Ici, this pointe sur le View.OnClickListener, on doit donc utiliser SearchActivity.this
+                Toast.makeText(SearchActivity.this, name.getText(), Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
